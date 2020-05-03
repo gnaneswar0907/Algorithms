6e69448c1b772a29bf51a5aa6afbffce0a5c46f8
gnaneswar0907 <gxg170000@utdallas.edu>
2020-05-03 03:49:59 -0500

    Day 1 Problem done

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -1,5 +1,14 @@
-public class FirstBadVersion {
-    public static void main(String[] args) {
-
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int min = 1, max = n;
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            if (isBadVersion(mid)) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return min;
     }
 }
