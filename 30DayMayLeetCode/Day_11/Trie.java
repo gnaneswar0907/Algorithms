ab55d7367741862da1953e5e9c7edd848d08e0fb
gnaneswar0907 <gxg170000@utdallas.edu>
2020-05-14 23:57:17 -0500

    Completed May 14 challenge

---
+++ 30DayMayLeetCode/Day_11/Trie.java
@@ -0,0 +1,97 @@
+public class Trie {
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!temp.containsKey(c)) {
+                temp.put(c, new TrieNode());
+            }
+            temp = temp.get(c);
+        }
+        temp.setEnd();
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!temp.containsKey(c)) {
+                return false;
+            }
+            temp = temp.get(c);
+        }
+        return temp.isEnd();
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String word) {
+        TrieNode temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!temp.containsKey(c)) {
+                return false;
+            }
+            temp = temp.get(c);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println(trie.search("app"));
+    }
+}
+
+class TrieNode {
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such: Trie obj = new
+ * Trie(); obj.insert(word); boolean param_2 = obj.search(word); boolean param_3
+ * = obj.startsWith(prefix);
+ */
