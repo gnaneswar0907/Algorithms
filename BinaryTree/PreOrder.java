b05a76028e800b8853ec258b6a1ae6a7afc59799
gnaneswar0907 <gxg170000@utdallas.edu>
2020-05-21 01:54:39 -0500

    Binary Tree Preorder completed

---
+++ BinaryTree/PreOrder.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import BinaryTree.TreeNode;
+
+public class PreOrder {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+
+        // Restrictive Solution
+        // if (root != null){
+        // l.add(root.val);
+        // l.addAll(preorderTraversal(root.left));
+        // l.addAll(preorderTraversal(root.right));
+        // }
+        // return result;
+
+        // Iterative Solution
+        Stack<TreeNode> stck = new Stack<>();
+        if (root == null)
+            return l;
+        stck.push(root);
+        while (!stck.isEmpty()) {
+            TreeNode temp = stck.pop();
+            l.add(temp.val);
+            if (temp.right != null)
+                stck.push(temp.right);
+            if (temp.left != null)
+                stck.push(temp.left);
+        }
+        return l;
+    }
+}
