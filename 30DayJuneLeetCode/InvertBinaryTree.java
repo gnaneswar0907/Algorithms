1328880b3ec17b2058045a5eafa6fe7405d234a8
gnaneswar0907 <gxg170000@utdallas.edu>
2020-06-01 22:15:18 -0500

    Completed June 1 Challenge

---
+++ 30DayJuneLeetCode/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+import BinaryTree.TreeNode;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return root;
+        if (root.left == null && root.right == null)
+            return root;
+        TreeNode temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+        return root;
+    }
+}
